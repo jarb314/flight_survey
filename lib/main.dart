e3a6cba43c4817ec9270ab793a8bbd8cd0a70409
Jose Angel Bautista Romero <52365128+jarb314@users.noreply.github.com>
2022-04-19 17:46:51 -0400

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
+    debugShowCheckedModeBanner: false,
     home: FlightsStepper(),
   ));
 }
